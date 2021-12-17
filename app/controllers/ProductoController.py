a408b73bbe52adc58ae560776d95836a08eb9bca
Marisa PA <81638335+Marisa1001@users.noreply.github.com>
2021-12-16 23:52:36 -0400

    productos

--- app/controllers/ProductoController.py
+++ app/controllers/ProductoController.py
@@ -1,13 +1,6 @@
 from flask import render_template, url_for, request, redirect, flash
 from app.models.Producto import Producto
-from app import db, app
-
-import os
-import time
-from PIL import Image #pip install pillow
-import urllib.request
-from werkzeug.utils import secure_filename
-
+from app import db
 class ProductoController():
     def __init__(self):
         pass
@@ -21,40 +14,12 @@ class ProductoController():
         if request.method == 'POST':
             nom_prod = request.form['nom_prod']
             costo = request.form['costo']
-            det_prod = request.form['det_prod']
-            picture_prod = 'user.png'
-            if 'file' not in request.files:
-                flash('No file part')
-                return redirect(request.url)
-            file = request.files['file']
-            if file.filename == '':
-                flash('No image selected for uploading')
-                return redirect(request.url)
-            if file: #and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                #guardar nombre
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                
-                fecha = time.strftime("%Y%m%d%H%M%S")
-                imgPath = app.config['UPLOAD_FOLDER'] + filename
-                img = Image.open(imgPath)
-                img.save('app/static/img/'+fecha+'.png')
-                os.remove('app/static/img/'+filename)
-                newfilename = fecha+'.png'
-                
-                
-                productoadd = Producto(nom_prod = nom_prod,costo=costo,det_prod=det_prod,picture_prod=newfilename)
-                db.session.add(productoadd)
-                db.session.commit()
-
-                
-                
-                flash('El registro se ha realizado con éxito.')
-                return redirect(url_for('producto_route.index'))
-            else:
-                flash('Allowed image types are -> png, jpg, jpeg, gif')
-                return redirect(request.url)
-            
+            #from app.models.Alumno import Alumno
+            productoadd = Producto(nom_prod = nom_prod, costo = costo)
+            db.session.add(productoadd)
+            db.session.commit()
+            flash('El nuevo productoo se ha registrado correctamente!!!')
+            return redirect(url_for('producto_route.index'))
     def delete(self, _id):
         producto = Producto.query.get(_id)
         db.session.delete(producto)
@@ -66,42 +31,11 @@ class ProductoController():
         return render_template('producto/edit.html',producto=producto)
     def update(self, _id):
         if request.method == 'POST':
-            productoV = request.form['nom_prod']
+            productoV = request.form['producto']
             productoDB = Producto.query.get(_id)
-            productoDB.nom_prod = productoV
-            productoV1 = request.form['costo']
-            productoDB = Producto.query.get(_id)
-            productoDB.costo = productoV1
-            productoV2 = request.form['det_prod']
-            productoDB = Producto.query.get(_id)
-            productoDB.det_prod = productoV2
-            if 'file' not in request.files:
-                flash('No file part')
-                return redirect(request.url)
-            file = request.files['file']
-            if file.filename == '':
-                flash('No image selected for uploading')
-                return redirect(request.url)
-            if file: #and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                #guardar nombre
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                
-                fecha = time.strftime("%Y%m%d%H%M%S")
-                imgPath = app.config['UPLOAD_FOLDER'] + filename
-                img = Image.open(imgPath)
-                img.save('app/static/img/'+fecha+'.png')
-                os.remove('app/static/img/'+filename)
-                newfilename = fecha+'.png'
-
-                producto =  Producto.query.get(_id)   
-                producto.picture_prod = newfilename
-                db.session.commit()
-                flash('El producto se ha actualizado con éxito.')
-                return redirect(url_for('producto_route.index'))
-            else:
-                flash('Allowed image types are -> png, jpg, jpeg, gif')
-                return redirect(request.url)
-            
+            productoDB.producto = productoV
+            db.session.commit()
+            flash('El registro se ha actualizado con éxito.')
+            return redirect(url_for('producto_route.index'))
 
 productocontroller = ProductoController()
