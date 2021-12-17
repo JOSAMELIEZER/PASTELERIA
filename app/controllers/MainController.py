ec50f2b6ab063eaf528a1710f13d1b944432c70a
Marisa PA <81638335+Marisa1001@users.noreply.github.com>
2021-12-16 23:50:54 -0400

    definir cliente

--- app/controllers/MainController.py
+++ app/controllers/MainController.py
@@ -9,8 +9,13 @@ class MainController():
         # user = {'name': 'Josam Pinaya'}
         user= User.query.get(1)
         return render_template('index.html', user=user)
-        
+
+    def client(self):
+        # user = {'name': 'Josam Pinaya'}
+        user= User.query.get(1)
+        return render_template('client.html', user=user)    
         #users = User.query.all()
         #return render_template('index.html', users=users)
         #return render_template('index.html')
+        
 maincontroller = MainController()
