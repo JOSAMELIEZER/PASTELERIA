1e939649e03460552375c4314cf196509570eb95
unknown <a.colq.c@hotmail.com>
2022-01-06 17:04:34 -0400

    agregado el boton eliminar para usuarios funcional

--- app/controllers/UsuarioController.py
+++ app/controllers/UsuarioController.py
@@ -1,6 +1,6 @@
 from app.models.Usuario import Usuario
 from app import db
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 class UsuarioController():
     def __init__(self):
         pass
@@ -12,5 +12,11 @@ class UsuarioController():
         """ from app.models.Usuario import Usuario
         usuarios = Usuario.query.all()
         return render_template('materias/index.html', usuarios=usuarios) """
+    def eliminar(self, _id):
+        usuario = Usuario.query.get(_id)
+        db.session.delete(usuario)
+        db.session.commit()
+        flash('El usuario '+ usuario.nombre +' se ha eliminado con éxito.')
+        return redirect(url_for('usuario_route.index'))
 
 usuariocontroller = UsuarioController()
--- app/routes/usuario_route.py
+++ app/routes/usuario_route.py
@@ -6,3 +6,7 @@ usuario_route = Blueprint('usuario_route', __name__)
 @usuario_route.route('/usuario',methods=['GET'])
 def index():
     return usuariocontroller.index()
+
+@usuario_route.route('/usuario/<int:id>/eliminar',methods=['GET'])
+def eliminar(id):
+    return usuariocontroller.eliminar(id)
